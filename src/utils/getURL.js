2f7a64154b261ad1303dfc56df7963745a5883ba
Mihkel Tiganik <mtiganik@gmail.com>
2023-09-10 12:53:21 +0300

    Typo in getURL

--- src/utils/getURL.js
+++ src/utils/getURL.js
@@ -1,7 +1,7 @@
 
 
 const getURL = () => {
-  return process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEVELOOP_URL : process.env.REACT_APP_PRODUCTION_URL
+  return process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEVELOP_URL : process.env.REACT_APP_PRODUCTION_URL
 
 }
 
